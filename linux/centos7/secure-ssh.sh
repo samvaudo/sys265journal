596572540ef360c956a0da83425248ea4dc0980d
samvaudo <samuel.vaudo@mymail.champlain.edu>
2024-02-28 19:27:05 +0000

    updated securessh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -1,6 +1,6 @@
-#secure-ssh.sh
-#author samvaudo
-#creates a new ssh user using $1 parameter
-#adds a public key from the local repo or curled from the remote repo
-#removes roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE"
+sudo useradd -m -d /home/sys265 -s /bin/bash sys265
+sudo mkdir /home/sys265/.ssh
+sudo cp /home/samuel/techjournal/sys265journal/linux/public-keys/id_rsa.pub /home/sys265/.ssh/authorized_keys
+sudo chmod 700 /home/sys265/.ssh
+sudo chmod 600 /home/sys265/.ssh/authorized_keys
+sudo chown -R sys265:sys265 /home/sys265/.ssh
